89b4a630e298d08149c4a072846270d5c73d45b0
Abel <eddy.gta17@gmail.com>
2020-05-04 13:01:26 +0530

    ToDo 1 Done

--- src/main.cpp
+++ src/main.cpp
@@ -55,11 +55,17 @@ int main(int argc, const char **argv)
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
+    float start_x,start_y,end_x,end_y;
+    std::cout<<"Enter the START co-ordinates :\t";
+    std::cin>>start_x>>start_y;
+    std::cout<<"Enter the END co-ordinates :\t";
+    std::cin>>end_x>>end_y;
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
+    // RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     RoutePlanner route_planner{model, 10, 10, 90, 90};
     route_planner.AStarSearch();
 
